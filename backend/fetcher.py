48c8154a6e55ef2b972dad12ac0baa9bfefb2f8b
talhasueluen-afk <talha.sueluen@gmail.com>
2026-05-22 11:46:35 +0200

    feat: add fetcher stub

---
+++ backend/fetcher.py
@@ -0,0 +1,12 @@
+MOCK_PV_DATA = {
+    "leistung_w": 3500,
+    "strom_a": 72.6,
+    "tagesertrag_kwh": 18.4,
+    "wirkungsgrad_pct": 75.0,
+    "co2_einsparung_kg": 2.4,
+    "timestamp": "2024-05-22T12:00:00",
+}
+
+def fetch_pv_data(url: str) -> dict:
+    """Fetches current PV data from the given URL."""
+    pass
